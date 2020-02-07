ba4ddd3ffd0f8fe0c1e7242a54fac3750c31afbc
alex-png <alexrod102797@gmail.com>
2020-02-07 11:40:27 -0500

    log in

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Container from './Main/Container'
-function App() {
-  return ( 
-    <Container />
-    );
-}
+import LogIn from './Main/Login.js'
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+class App extends React.Component {
+
+  state = {
+    loggedIn: false
+  }
+
+
+
+  render() {
+    return(this.state.loggedIn ? <Container /> : <LogIn />)
+  }
+  
+}//
 
 export default App;
--- src/Main/Login.js
+++ src/Main/Login.js
@@ -1,10 +1,21 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import React from "react"
 
 export default class LogIn extends React.Component {
 
 
     render(){
-        <h1>Hello mate</h1>
+        return(
+        <>
+            
+            <form style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", marginTop:" 20vh" }}> 
+                <h1>Log In</h1>
+                <input placeholder="Username...">
+                </input>
+                <input placeholder="Password..."></input>
+                <button>Log in</button>
+            </form>
+        </>
+        )
+
     }
 }
